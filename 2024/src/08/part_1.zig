c1c157458ad6d63e7dd7b9fedb5f89d13f828a3f
Moritz Gottschling <gottschling.moritz@gmail.com>
2024-12-09 12:50:36 +0100

    Improve day 8

--- 2024/src/08/part_1.zig
+++ 2024/src/08/part_1.zig
@@ -5,50 +5,45 @@ const pretty = @import("pretty");
 const common = @import("common");
 
 pub fn run(allocator: std.mem.Allocator, input: parse.Input) !u64 {
-    var antinodes = std.ArrayList(parse.Vector2D).init(allocator);
-    defer antinodes.deinit();
+    var unique_antinodes = std.AutoHashMap(parse.Vector2D, void).init(allocator);
+    defer unique_antinodes.deinit();
 
     var iter = input.antennas.valueIterator();
     while (iter.next()) |antennas| {
-        const new_antinodes = try calculateAntinodesForFrequency(allocator, antennas.items, input);
-        defer allocator.free(new_antinodes);
-        try antinodes.appendSlice(new_antinodes);
-    }
-
-    var unique_antinodes = std.AutoHashMap(parse.Vector2D, void).init(allocator);
-    defer unique_antinodes.deinit();
-    for (antinodes.items) |antinode| {
-        try unique_antinodes.put(antinode, {});
+        try calculateAntinodesForFrequency(antennas.items, input, &unique_antinodes);
     }
 
     return unique_antinodes.count();
 }
 
 fn calculateAntinodesForFrequency(
-    allocator: std.mem.Allocator,
     antennas: []parse.Vector2D,
     input: parse.Input,
-) ![]parse.Vector2D {
-    var antinodes = std.ArrayList(parse.Vector2D).init(allocator);
+    unique_antinodes: *std.AutoHashMap(parse.Vector2D, void),
+) !void {
     for (0..antennas.len) |i| {
         for (i + 1..antennas.len) |j| {
-            const antinode = calculateAntinodesForAntennas(antennas[i], antennas[j], input);
-            if (antinode.antinode_a) |n| try antinodes.append(n);
-            if (antinode.antinode_b) |n| try antinodes.append(n);
+            try calculateAntinodesForAntennas(
+                antennas[i],
+                antennas[j],
+                input,
+                unique_antinodes,
+            );
         }
     }
-    return antinodes.toOwnedSlice();
 }
 
 fn calculateAntinodesForAntennas(
     antenna_a: parse.Vector2D,
     antenna_b: parse.Vector2D,
     input: parse.Input,
-) struct { antinode_a: ?parse.Vector2D, antinode_b: ?parse.Vector2D } {
+    unique_antinodes: *std.AutoHashMap(parse.Vector2D, void),
+) !void {
     const direction = antenna_b.sub(antenna_a);
 
     const antinode_a = antenna_a.subSafe(direction, input) catch null;
     const antinode_b = antenna_b.addSafe(direction, input) catch null;
 
-    return .{ .antinode_a = antinode_a, .antinode_b = antinode_b };
+    if (antinode_a) |antinode| try unique_antinodes.put(antinode, {});
+    if (antinode_b) |antinode| try unique_antinodes.put(antinode, {});
 }
--- 2024/src/08/part_2.zig
+++ 2024/src/08/part_2.zig
@@ -5,67 +5,50 @@ const pretty = @import("pretty");
 const common = @import("common");
 
 pub fn run(allocator: std.mem.Allocator, input: parse.Input) !u64 {
-    var antinodes = std.ArrayList(parse.Vector2D).init(allocator);
-    defer antinodes.deinit();
+    var unique_antinodes = std.AutoHashMap(parse.Vector2D, void).init(allocator);
+    defer unique_antinodes.deinit();
 
     var iter = input.antennas.valueIterator();
     while (iter.next()) |antennas| {
-        const new_antinodes = try calculateAntinodesForFrequency(allocator, antennas.items, input);
-        defer allocator.free(new_antinodes);
-        try antinodes.appendSlice(new_antinodes);
-    }
-
-    var unique_antinodes = std.AutoHashMap(parse.Vector2D, void).init(allocator);
-    defer unique_antinodes.deinit();
-    for (antinodes.items) |antinode| {
-        try unique_antinodes.put(antinode, {});
+        try calculateAntinodesForFrequency(allocator, antennas.items, input, &unique_antinodes);
     }
 
     return unique_antinodes.count();
 }
 
 fn calculateAntinodesForFrequency(
-    allocator: std.mem.Allocator,
+    _: std.mem.Allocator,
     antennas: []parse.Vector2D,
     input: parse.Input,
-) ![]parse.Vector2D {
-    var antinodes = std.ArrayList(parse.Vector2D).init(allocator);
+    unique_antinodes: *std.AutoHashMap(parse.Vector2D, void),
+) !void {
     for (0..antennas.len) |i| {
         for (i + 1..antennas.len) |j| {
-            var pair_antinodes = try calculateAntinodesForAntennas(allocator, antennas[i], antennas[j], input);
-            defer pair_antinodes.deinit();
-            try antinodes.appendSlice(pair_antinodes.items);
+            try calculateAntinodesForAntennas(antennas[i], antennas[j], input, unique_antinodes);
         }
     }
-
-    return antinodes.toOwnedSlice();
 }
 
 fn calculateAntinodesForAntennas(
-    allocator: std.mem.Allocator,
     antenna_a: parse.Vector2D,
     antenna_b: parse.Vector2D,
     input: parse.Input,
-) !std.ArrayList(parse.Vector2D) {
-    var antinodes = std.ArrayList(parse.Vector2D).init(allocator);
-    errdefer antinodes.deinit();
-
+    unique_antinodes: *std.AutoHashMap(parse.Vector2D, void),
+) !void {
     const direction = antenna_b.sub(antenna_a);
     const norm_direction = normalizeVector(direction);
 
     var current = antenna_a;
     while (true) {
-        try antinodes.append(current);
+        try unique_antinodes.put(current, {});
         current = current.subSafe(norm_direction, input) catch break;
     }
 
     current = antenna_b;
     while (true) {
-        try antinodes.append(current);
+        try unique_antinodes.put(current, {});
         current = current.addSafe(norm_direction, input) catch break;
     }
-
-    return antinodes;
 }
 
 fn normalizeVector(vector: parse.Vector2D) parse.Vector2D {
